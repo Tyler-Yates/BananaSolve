9b29b9419080c709dd52cd5ffdc8339bc2e7de50
Tyler Yates <tyler.yates@utexas.edu>
2017-05-29 00:42:51 -0500

    Improve AbstractTextFileDictionary

--- src/main/java/tyates/bananasolve/dictionary/AbstractTextFileDictionary.java
+++ src/main/java/tyates/bananasolve/dictionary/AbstractTextFileDictionary.java
@@ -6,10 +6,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import static tyates.bananasolve.util.StringStandardizer.standardize;
 
 public abstract class AbstractTextFileDictionary implements Dictionary {
+    private static final Pattern ALPHABET_CHARS_ONLY = Pattern.compile("[a-z]+");
+
     private final Set<String> validWords;
 
     AbstractTextFileDictionary(final String resourceFileName) throws FileNotFoundException {
@@ -18,13 +21,16 @@ public abstract class AbstractTextFileDictionary implements Dictionary {
 
         final ImmutableSet.Builder<String> wordBuilder = ImmutableSet.builder();
         while (scanner.hasNext()) {
-            wordBuilder.add(standardize(scanner.nextLine()));
+            final String word = standardize(scanner.nextLine());
+            if (ALPHABET_CHARS_ONLY.matcher(word).matches()) {
+                wordBuilder.add(word);
+            }
         }
         validWords = wordBuilder.build();
     }
 
     @Override
-    public final boolean isValidWord(String word) {
+    public final boolean isValidWord(final String word) {
         return validWords.contains(standardize(word));
     }
 
