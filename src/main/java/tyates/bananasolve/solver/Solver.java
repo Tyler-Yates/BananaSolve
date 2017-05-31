4dcb3f4efd1e3947dbf81096a9c38087d9dd7cf7
Tyler Yates <tyler.yates@utexas.edu>
2017-05-31 17:22:55 -0500

    Create interface for Solver

--- src/main/java/tyates/bananasolve/solver/Solver.java
+++ src/main/java/tyates/bananasolve/solver/Solver.java
@@ -1,31 +1,17 @@
 package tyates.bananasolve.solver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
+import tyates.bananasolve.data.Board;
+import tyates.bananasolve.data.TileHand;
 
-import static tyates.bananasolve.util.StringStandardizer.standardize;
-
-public class Solver {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter tiles separated by commas: ");
-        final String input = reader.readLine();
-        final String[] parts = input.split(",");
-        final List<Character> tiles = new ArrayList<>();
-        for (final String part : parts) {
-            final String tile = standardize(part);
-            if (Pattern.matches("[a-z]", tile)) {
-                tiles.add(tile.charAt(0));
-            } else {
-                System.err.printf("Invalid tile! '%s' is not a recognized tile\n", tile);
-                return;
-            }
-        }
-
-        System.out.println("Tiles: " + tiles);
-    }
+/**
+ * Interface for classes that solve a Bananagram hand.
+ */
+public interface Solver {
+    /**
+     * Returns a solution for the given hand of tiles.
+     *
+     * @param tiles the given hand of tiles
+     * @return a solution
+     */
+    Board solve(final TileHand tiles);
 }
