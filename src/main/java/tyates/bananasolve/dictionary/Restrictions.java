f3fcd9a485710c8fc7ebe80495d221f953a62738
Tyler Yates <tyler.yates@utexas.edu>
2017-06-03 18:45:15 -0500

    Create Restriction for word containing a character

--- src/main/java/tyates/bananasolve/dictionary/Restrictions.java
+++ src/main/java/tyates/bananasolve/dictionary/Restrictions.java
@@ -30,4 +30,19 @@ public final class Restrictions {
             }
         };
     }
+
+    /**
+     * Returns a restriction that requires each word to contain the given character.
+     *
+     * @param ch the given character
+     * @return the restriction
+     */
+    public static Restriction contains(final char ch) {
+        return new Restriction() {
+            @Override
+            public boolean permissible(String word) {
+                return word.contains("" + ch);
+            }
+        };
+    }
 }
